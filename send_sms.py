0a1e72fddf2fde21982b20ae43312cc8b408e643
itsregalo <itsregalo047@gmail.com>
2023-12-13 20:44:36 +0300

    Fix sms workdlow

--- send_sms.py
+++ send_sms.py
@@ -1,11 +1,14 @@
 import africastalking
 import os
+import sys
 
 AT_API_KEY = os.environ['API_KEY']
 AT_USERNAME = os.environ['USERNAME']
 AT_PHONE_NUMBER = os.environ['PHONE_NUMBER']
+MESSAGE = sys.argv[1]
 
 africastalking.initialize(AT_USERNAME, AT_API_KEY)
 
 sms = africastalking.SMS
-response = sms.send(message="Workflows was successful", recipients=[f"{AT_PHONE_NUMBER}"])
+response = sms.send(message=MESSAGE, 
+                    recipients=[f"{AT_PHONE_NUMBER}"])
